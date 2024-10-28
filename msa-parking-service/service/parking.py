436bc73b2937befe16730e07854975c72768b430
yoon741 <mono74140@gmail.com>
2024-10-28 22:46:24 +0900

    parking service - trigger 구문 수정 / 프로젝트 구조 수정

--- msa-parking-service/service/parking.py
+++ msa-parking-service/service/parking.py
@@ -8,37 +8,46 @@ from service.database import db_url
 
 engine = create_engine(db_url, echo=True)
 
-
 def create_triggers():
+    # 차량번호 입력 해 parking 테이블에 저장하면 trigger 작동 (parkseat에 값 저장)
+    trigger_sql_parking = """
+    CREATE TRIGGER IF NOT EXISTS after_insert_parking
+    AFTER INSERT ON parking
+    FOR EACH ROW
+    BEGIN
+        INSERT INTO parkseat (carnum, barrier) VALUES (NEW.carnum, NEW.barrier);
+    END;
+    """
+
+    # 결제완료 시 payment 테이블에 저장 된 paydate에 값이 채워지면
+    # parkseat에 동일한 carnum 데이터 삭제
+    trigger_sql_payment = """
+    CREATE TRIGGER IF NOT EXISTS remove_parkseat
+    AFTER UPDATE ON payment
+    FOR EACH ROW
+    WHEN OLD.paydate IS NULL AND NEW.paydate IS NOT NULL
+    BEGIN
+        DELETE FROM parkseat WHERE carnum = NEW.carnum;
+    END;
+    """
+
     with engine.connect() as connection:
-        result = connection.execute(text("SELECT name FROM sqlite_master WHERE type='trigger' AND name='after_insert_parking'"))
-        if not result.fetchone():
-            # 차량번호 입력 해 parking 테이블에 저장하면 trigger 작동 (parkseat에 값 저장)
-            trigger_sql_parking = """
-            CREATE TRIGGER insert_parking
-            AFTER INSERT ON parking
-            FOR EACH ROW
-            BEGIN
-                INSERT INTO parkseat (carnum, barrier) VALUES (NEW.carnum, NEW.barrier);
-            END;
-            """
+        # 주차 트리거가 존재하는지 확인 후 생성
+        existing_trigger_parking = connection.execute(
+            text("SELECT name FROM sqlite_master WHERE type='trigger' AND name='after_insert_parking'")
+        ).fetchone()
+
+        if not existing_trigger_parking:
             connection.execute(text(trigger_sql_parking))
 
-        result = connection.execute(text("SELECT name FROM sqlite_master WHERE type='trigger' AND name='remove_parkseat'"))
-        if not result.fetchone():
-            # 결제완료 시 payment 테이블에 저장 된 paydate에 값이 채워지면
-            # parkseat에 동일한 carnum 데이터 삭제
-            trigger_sql_payment = """
-            CREATE TRIGGER remove_parkseat
-            AFTER UPDATE ON payment
-            FOR EACH ROW
-            BEGIN
-                IF OLD.paydate IS NULL AND NEW.paydate IS NOT NULL THEN
-                    DELETE FROM parkseat WHERE carnum = NEW.carnum;
-                END IF;
-            END;
-            """
+        # 결제 트리거가 존재하는지 확인 후 생성
+        existing_trigger_payment = connection.execute(
+            text("SELECT name FROM sqlite_master WHERE type='trigger' AND name='remove_parkseat'")
+        ).fetchone()
+
+        if not existing_trigger_payment:
             connection.execute(text(trigger_sql_payment))
+
 
 # 입차
 # 차량번호 입력 시 parking테이블에 저장
@@ -72,16 +81,16 @@ def set_outtime(db: Session, carnum: str):
     db.commit()
 
 # 차량 정보 출력
-    # total_time = parking.outtime - parking.intime
-    # total_minutes = total_time.total_seconds() / 60
-    #
-    # rate_10min = 1500       # 회차시간 15분 / 10분당 1500원
-    # total_fee = int((max(0, total_minutes - 15) / 10) * rate_10min)
-    #
-    # return {
-    #     "carnum": parking.carnum,
-    #     "intime": parking.intime,
-    #     "outtime": parking.outtime,
-    #     "total_minutes": total_minutes,
-    #     "total_fee": total_fee
-    # }
+# total_time = parking.outtime - parking.intime
+# total_minutes = total_time.total_seconds() / 60
+#
+# rate_10min = 1500       # 회차시간 15분 / 10분당 1500원
+# total_fee = int((max(0, total_minutes - 15) / 10) * rate_10min)
+#
+# return {
+#     "carnum": parking.carnum,
+#     "intime": parking.intime,
+#     "outtime": parking.outtime,
+#     "total_minutes": total_minutes,
+#     "total_fee": total_fee
+# }
